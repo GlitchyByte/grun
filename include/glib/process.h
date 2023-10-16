e964daad3e18d3291a3a63cc5c6de5f0c9b606fb
Luis Mejia <lmejia@gmail.com>
2023-10-15 20:20:46 -0700

    Added redirect of stderr to stdout when capturing output.

--- include/glib/process.h
+++ include/glib/process.h
@@ -7,7 +7,22 @@
 
 namespace glib::process {
 
-    bool execute(const std::string& command, const std::optional<std::filesystem::path>& workDir = std::nullopt,
+    /**
+     * Executes a command and optionally captures it's output.
+     *
+     * <p>Output can be filtered before capture, allowing the caller to even process the lines
+     * as they come and not capture. It is advised that the filter lambda be as fast as possible
+     * so as to not hold the executed process.
+     *
+     * @param command Full command line as it would be on a terminal.
+     * @param workDir Directory to run the command on.
+     * @param lines Optional vector to accumulate lines into.
+     * @param exitCode Optional exit code from command.
+     * @param filter Optional lambda to filter or process each line.
+     *              filter must return true to add the line to lines, false to skip.
+     * @return True if command execution was successful.
+     */
+    bool execute(const std::string_view& command, const std::optional<std::filesystem::path>& workDir = std::nullopt,
             std::vector<std::string>* lines = nullptr, int* exitCode = nullptr,
             const std::function<bool(const std::string_view&)>& filter = nullptr);
 }
--- src/glib/process.cpp
+++ src/glib/process.cpp
@@ -1,18 +1,21 @@
 // Copyright 2023 GlitchyByte
 // SPDX-License-Identifier: Apache-2.0
 
+#include <sstream>
 #include "glib/process.h"
 
 namespace glib::process {
 
-    bool execute(const std::string& command, const std::optional<std::filesystem::path>& workDir,
+    bool execute(const std::string_view& command, const std::optional<std::filesystem::path>& workDir,
             std::vector<std::string>* lines, int* exitCode,
             const std::function<bool(const std::string_view&)>& filter) {
         const auto& originalDir = std::filesystem::current_path();
         if (workDir.has_value()) {
             std::filesystem::current_path(workDir.value());
         }
-        FILE* pipe = popen(command.c_str(), "r");
+        std::ostringstream redirectedCommand;
+        redirectedCommand << command << " 2>&1";
+        FILE* pipe = popen(redirectedCommand.str().c_str(), "r");
         if (!pipe) {
             std::filesystem::current_path(originalDir);
             return false;
